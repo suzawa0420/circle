92cbe891895263c93984233723700a5c30eb57a2
suzawa0420 <suzawa@photomo.jp>
2026-09-22 21:46:21 +0900

    お問い合わせメールの運営宛BCCを削除

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
   default from:     "noreply@circle-book.com",
-          bcc:      "circlebook26@gmail.com",
           reply_to: "circlebook26@gmail.com"
   layout 'mailer'
 end
