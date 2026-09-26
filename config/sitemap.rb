999d3910b1b5eaa28d0ff700c64328aa7d73b279
suzawa0420 <suzawa@photomo.jp>
2026-09-26 19:14:35 +0900

    サイトマップ更新でS3 SDKを明示的に読み込む

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,3 +1,5 @@
+require 'aws-sdk-s3'
+
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = 'https://circle-book.com'
 SitemapGenerator::Sitemap.sitemaps_host = "https://s3-ap-northeast-1.amazonaws.com/#{ENV['AWS_S3_BUCKET']}"
